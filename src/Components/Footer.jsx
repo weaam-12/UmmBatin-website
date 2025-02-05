c27e485d97f521e365a0c7e57a0f39de5714126e
weaam-12 <abukafweaam2001@gmail.com>
2025-02-05 10:13:57 +0200

    Add only frontend and backend directories

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer>
-        <p>צור קשר: info@example.com</p>
-    </footer>
-);
-
-export default Footer;
--- src/main/java/finalproject/webproject/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package finalproject.webproject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestRepository extends JpaRepository<Request, Long> {
-    // You can define custom queries here if needed
-}
